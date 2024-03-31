b7c5a2af708b4d3756c4c6305d0c2c139b51052a
eldzntm <jjson1211092@gmail.com>
2024-03-31 15:37:03 +0900

    주 등급에따른 월당 인구증가 가속, 특정주에서는 랜덤이벤트x,60년 시작

--- common/defines/ATF_defines.lua
+++ common/defines/ATF_defines.lua
@@ -1,3 +1,6 @@
-NDefines.NResistance.FOREIGN_MANPOWER_MIN_THRESHOLD = 0;
+NDefines.NGame.START_DATE = "100.1.1.12";
+NDefines.NGame.END_DATE = "2000.1.1.1";
 NDefines.NCountry.MIN_STABILITY = -1.0;
 NDefines.NCountry.MAX_STABILITY = 1.0;
+NDefines.NCountry.MIN_WAR_SUPPORT = -1.0;
+NDefines.NCountry.MAX_WAR_SUPPORT = 1.0;
